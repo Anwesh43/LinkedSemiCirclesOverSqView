fbea215ff2da90523f8fea27aeb52a468a318335
Anwesh43 <anweshthecool0@gmail.com>
2019-05-14 08:41:00 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsemicirclesoversqview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsemicirclesoversqview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsemicirclesoversqview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.semicirclesoversqview.SemiCirclesOverSqView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SemiCirclesOverSqView.create(this)
     }
 }
