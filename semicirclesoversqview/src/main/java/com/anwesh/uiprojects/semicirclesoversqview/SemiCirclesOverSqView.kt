16d2ba4eb40a17161d43b150fad2085aa384ea4d
Anwesh43 <anweshthecool0@gmail.com>
2019-05-14 08:35:51 +0530

    adding SemiCirclesOverSq class

--- semicirclesoversqview/src/main/java/com/anwesh/uiprojects/semicirclesoversqview/SemiCirclesOverSqView.kt
+++ semicirclesoversqview/src/main/java/com/anwesh/uiprojects/semicirclesoversqview/SemiCirclesOverSqView.kt
@@ -166,4 +166,28 @@ class SemiCirclesOverSqView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SemiCirclesOverSq(var i : Int) {
+
+        private val root : SCONode = SCONode(0)
+        private var curr : SCONode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
